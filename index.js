89fc064738d65c5497ceaf34c5c0dac02a88b0f6
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-11-11 18:17:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
 let welcomeMessage = "Welcome to Flatbook, ${currentUser} !";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
